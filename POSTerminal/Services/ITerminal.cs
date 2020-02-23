9329bb67b661572a283fb53c973c7dc92fb5aca1
Yaroslav Tykhonchuk <yaroslav.tykhonchuk@poort80.nl>
2020-02-24 00:31:24 +0200

    Designed terminal service

---
+++ POSTerminal/Services/ITerminal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using POSTerminal.Models;
+
+namespace POSTerminal.Services
+{
+    public interface ITerminal
+    {
+        void SetPricing(IEnumerable<Product> products);
+
+        void Scan(string productCode);
+
+        decimal CalculateTotal();
+    }
+}
---
+++ POSTerminal/Services/ShopTerminal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using POSTerminal.Models;
+
+namespace POSTerminal.Services
+{
+    public class ShopTerminal : ITerminal
+    {
+        private IEnumerable<Product> _products;
+
+        public void SetPricing(IEnumerable<Product> products)
+        {
+            _products = products;
+        }
+
+        public void Scan(string productCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
