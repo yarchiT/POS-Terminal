a1ee65bfcd61ab5864292d8dd1967d36de11cf41
Yaroslav Tykhonchuk <yaroslav.tykhonchuk@poort80.nl>
2020-02-24 00:11:48 +0200

    Init Product model

---
+++ POSTerminal/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace POSTerminal.Models
+{
+    public class Product
+    {
+        public string Code { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int? NumberForVolume { get; set; }
+
+        public decimal? VolumePrice { get; set; }
+    }
+}
