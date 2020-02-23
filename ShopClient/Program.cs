32bfd117fd4d6f119a5b46e1ac546d4cb53ff7cf
Yaroslav Tykhonchuk <yaroslav.tykhonchuk@poort80.nl>
2020-02-24 00:09:45 +0200

    Init commit

---
+++ ShopClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShopClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
