180c766d0a783bc7b66739bf067869101cb1ea44
Yaroslav Tykhonchuk <yaroslav.tykhonchuk@poort80.nl>
2020-02-24 22:38:14 +0200

    Added DiscountCard model

---
+++ POSTerminal/Models/DiscountCard.cs
@@ -0,0 +1,9 @@
+﻿namespace POSTerminal.Models
+{
+    public class DiscountCard
+    {
+        public decimal DiscountPercent { get; set; }
+
+        public decimal AmountOfSale { get; set; }
+    }
+}
