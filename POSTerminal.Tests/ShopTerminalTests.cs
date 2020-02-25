0da42d74627248e725b3fefbcd0f9b0c2b92e1ed
Yaroslav Tykhonchuk <yaroslav.tykhonchuk@poort80.nl>
2020-02-25 22:23:16 +0200

    Added ShopTerminal.Tests project

---
+++ POSTerminal.Tests/ShopTerminalTests.cs
@@ -0,0 +1,18 @@
+using POSTerminal.Services.DiscountService;
+using POSTerminal.Services.PricingService;
+using Xunit;
+
+namespace POSTerminal.Tests
+{
+    public class ShopTerminalTests
+    {
+        private readonly IPricingService _pricingService;
+        private readonly IDiscountService _discountService;
+
+        public ShopTerminalTests()
+        {
+
+        }
+
+    }
+}
