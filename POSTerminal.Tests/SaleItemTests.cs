c5a5abd7516a0978bdef4d1a960047127639b1b3
Yaroslav Tykhonchuk <y.tykhonchuk@draftkings.com>
2021-06-06 11:30:25 +0300

    Add Sale Item

---
+++ POSTerminal.Tests/SaleItemTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace POSTerminal.Tests
+{
+    public class SaleItemTests
+    {
+        [Fact]
+        public void Total_SingleItem_ReturnsUnitPrice()
+        {
+            var saleItem = new SaleItem(new Product("A", 1));
+
+            Assert.Equal(1, saleItem.Total());
+        }
+
+        [Fact]
+        public void Total_AfterIncrement_ReturnsUnitPrice()
+        {
+            var saleItem = new SaleItem(new Product("A", 1));
+            saleItem.Increment();
+
+            Assert.Equal(2, saleItem.Total());
+        }
+
+        [Fact]
+        public void Total_WithVolumeDiscount_ReturnsVolumePrice()
+        {
+            var saleItem = new SaleItem(new Product("A", 1, new VolumeDiscount(2, 3)));
+            saleItem.Increment();
+            saleItem.Increment();
+
+            Assert.Equal(2, saleItem.Total());
+        }
+
+        [Fact]
+        public void Total_WithVolumeDiscount_ReturnsCombinedPrice()
+        {
+            var saleItem = new SaleItem(new Product("A", 1, new VolumeDiscount(2, 3)));
+            saleItem.Increment();
+            saleItem.Increment();
+            saleItem.Increment();
+
+            Assert.Equal(3, saleItem.Total());
+        }
+    }
+}
---
+++ POSTerminal/SaleItem.cs
@@ -0,0 +1,18 @@
+﻿namespace POSTerminal
+{
+    public class SaleItem
+    {
+        public readonly Product Product;
+        private int _quantity = 1;
+
+        public SaleItem(Product product)
+        {
+            Product = product;
+        }
+
+        public void Increment() => _quantity++;
+
+        public decimal Total() =>
+            Product.VolumeDiscount?.GetTotalPrice(_quantity, Product.Price) ?? Product.Price * _quantity;
+    }
+}
