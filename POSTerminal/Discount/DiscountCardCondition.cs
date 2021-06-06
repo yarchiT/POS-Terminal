3fd0f3ce3e93313517652a2a75c7675f289d2be7
Yaroslav Tykhonchuk <y.tykhonchuk@draftkings.com>
2021-06-06 11:58:29 +0300

    Add Discount card condition class

---
+++ POSTerminal/Discount/DiscountCardCondition.cs
@@ -0,0 +1,16 @@
+﻿namespace POSTerminal.Discount
+{
+    public class DiscountCardCondition
+    {
+        public readonly decimal Percent;
+        public readonly decimal AmountFrom;
+        public readonly decimal? AmountTo;
+
+        public DiscountCardCondition(decimal percent, decimal amountFrom, decimal? amountTo = null)
+        {
+            Percent = percent;
+            AmountFrom = amountFrom;
+            AmountTo = amountTo;
+        }
+    }
+}
