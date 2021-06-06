ac25ed2658119a2c2e38c17a10ada91c1e9859d8
Yaroslav Tykhonchuk <y.tykhonchuk@draftkings.com>
2021-06-06 11:23:01 +0300

    Add volume discount tests

---
+++ POSTerminal.Tests/VolumeDiscountTests.cs
@@ -0,0 +1,31 @@
+using Xunit;
+
+namespace POSTerminal.Tests
+{
+    public class VolumeDiscountTests
+    {
+        [Fact]
+        public void GetTotalPrice_WithVolumeQuantity_ReturnsVolumePrice()
+        {
+            var volumeDiscount = new VolumeDiscount(5, 6);
+
+            Assert.Equal(5, volumeDiscount.GetTotalPrice(6, 1));
+        }
+
+        [Fact]
+        public void GetTotalPrice_MoreThanVolumeQuantity_ReturnsCombinedPrice()
+        {
+            var volumeDiscount = new VolumeDiscount(5, 6);
+
+            Assert.Equal(6, volumeDiscount.GetTotalPrice(7, 1));
+        }
+
+        [Fact]
+        public void GetTotalPrice_LessThanVolumeQuantity_ReturnsUnitPrice()
+        {
+            var volumeDiscount = new VolumeDiscount(5, 6);
+
+            Assert.Equal(4, volumeDiscount.GetTotalPrice(4, 1));
+        }
+    }
+}
