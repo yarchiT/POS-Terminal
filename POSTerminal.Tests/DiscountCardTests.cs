6b1cef32a81684668fa8369337a5869924e39aa2
Yaroslav Tykhonchuk <y.tykhonchuk@draftkings.com>
2021-06-06 12:11:01 +0300

    Add Discount card

---
+++ POSTerminal.Tests/DiscountCardTests.cs
@@ -0,0 +1,36 @@
+﻿using POSTerminal.Discount;
+using Xunit;
+
+namespace POSTerminal.Tests
+{
+    public class DiscountCardTests
+    {
+        [Fact]
+        public void GetTotalPrice_WithoutDiscountPercent_ReturnsUnitPrice()
+        {
+            var discountCard = new DiscountCard();
+
+            Assert.Equal(10, discountCard.GetTotalPrice(5, 2));
+        }
+
+        [Fact]
+        public void GetTotalPrice_WithDiscountPercent_ReturnsReducedPrice()
+        {
+            var discountCard = new DiscountCard(1500);
+
+            Assert.Equal(990, discountCard.GetTotalPrice(100, 10));
+        }
+
+        [Fact]
+        public void AddCurrentSale_AfterAddingLastSale_AppliesNewDiscountPercent()
+        {
+            var discountCard = new DiscountCard(1500);
+            var totalPriceWithOnePercentDiscount = discountCard.GetTotalPrice(100, 10);
+
+            discountCard.AddCurrentSale(1000);
+
+            Assert.Equal(990, totalPriceWithOnePercentDiscount);
+            Assert.Equal(970, discountCard.GetTotalPrice(100, 10));
+        }
+    }
+}
---
+++ POSTerminal/Discount/DiscountCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace POSTerminal.Discount
+{
+    public class DiscountCard : Discount
+    {
+        private List<DiscountCardCondition> _conditions = new List<DiscountCardCondition>
+        {
+            new DiscountCardCondition(0, 0, 999),
+            new DiscountCardCondition(1, 1000, 1999),
+            new DiscountCardCondition(3, 2000, 4999),
+            new DiscountCardCondition(5, 5000, 9999),
+            new DiscountCardCondition(7, 9999)
+        };
+
+        private decimal _amount;
+
+        public DiscountCard()
+        {
+            _amount = 0;
+        }
+
+        public DiscountCard(decimal amount)
+        {
+            _amount = amount;
+        }
+
+        public void AddCurrentSale(decimal amount) =>
+            _amount += amount;
+
+        public override decimal GetTotalPrice(int itemsQuantity, decimal unitPrice) =>
+            unitPrice * itemsQuantity - unitPrice * itemsQuantity * GetCurrentPercent()/100;
+
+        private decimal GetCurrentPercent() =>
+            _conditions.Single(c => c.AmountFrom <= _amount && (c.AmountTo == null || _amount < c.AmountTo)).Percent;
+    }
+}
